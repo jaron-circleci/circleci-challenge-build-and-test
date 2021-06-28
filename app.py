56271d767c4cc098a8484da91932db041a520d9f
Manyama, Jaron <jaron.manyama@concur.com>
2021-06-28 15:53:07 -0700

    update

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from flask.helpers import url_for
 
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def my_form():
     question = "What is the best CI/CD Tool?"
+    circleci_message="CircleCI!"
+    if request.method == 'POST':
+         return render_template('index.html', circleci_message=circleci_message)
+
     return render_template('index.html', question=question)
 
 
-@app.route('/process', methods=['POST'])
+@app.route('/process', methods= ['POST'])
 def my_form_post():
-    submitButton = request.form.get('Submit')
-    circleci_message="CircleCI!"
-    if submitButton:
-        return render_template('index.html', circleci_message=circleci_message)
+        submitButton = request.form.get('Submit')
+        if submitButton:
+            return redirect(url_for('index'))
 
     
--- test.py
+++ test.py
@@ -1,21 +1,16 @@
 import unittest
 import requests
-from . app import my_form_post
+import app
 
 class TestMyForm(unittest.TestCase):
-    '''
+    
     def setUp(self):
-        self.app = my_form_post.test_client()
+        self.app = app.app.test_client()
         self.app.testing = True
-    '''
+        
     def test_text(self):
-        API_URL = " http://127.0.0.1:5000/process"
-        r = requests.post(API_URL)
-        self.assertTrue(r'CircleCI' in r.text)
-    '''
-    def text_text_1(self):
-        circleci_message = 'CircleCi!'
-        self.assertEqual(self.my_form_post(), circleci_message)
-    '''
+        response = self.app.get('/', follow_redirects=True)
+        self.assertTrue(response.data, "CircleCI!" )
+    
 if __name__ == "__main__":
     unittest.main()
