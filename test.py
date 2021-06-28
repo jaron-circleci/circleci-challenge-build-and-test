9fd0dca5b40b785132d93fa36085560ebca6595d
Manyama, Jaron <jaron.manyama@concur.com>
2021-06-28 09:55:37 -0700

    added ci config

--- test.py
+++ test.py
@@ -4,10 +4,9 @@ import requests
 class TestMyForm(unittest.TestCase):
 
     def test_text(self):
-        API_URL = "http://localhost:5000/process"
+        API_URL = "/process"
         r = requests.post(API_URL)
         self.assertTrue(r'CircleCI' in r.text)
-
 
 
 if __name__ == "__main__":
